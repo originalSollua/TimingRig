5fa8f54e548490c73b7882debbc16499c2f279a0
Edward <pryoreb197@potsdam.edu>
2015-06-16 12:50:45 -0400

    times execution, but not the execution of the target program.

--- timing_rig.py
+++ timing_rig.py
@@ -1,6 +1,6 @@
 # intending this to be a rig to hold a python script to run timing on other programs.
 # flags:
-import os, sys
+import os, sys, time
 
 print "welcome to timing rig v0.1"
 arglist = sys.argv
@@ -16,9 +16,12 @@ else:
     print path
     path = path[:-5]
     print path
+    t = time.clock()
     var = os.system("java "+path)
+    e = time.clock()
     if not var == 0:
         sys.exit(path +" could not be run.")
+    print "Elapsd Execution time: "+str(e - t)+" Sec"
     sys.exit(0)
        
 
